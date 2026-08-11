63c52282dfc68b54a6bc2109520a5dbab3fc8a52
Seperatebugle21 <151967822+Seperatebugle21@users.noreply.github.com>
2026-08-11 20:20:45 +0200

    Fix confirmation message text formatting

--- Echo.swiftpm/Echo/Views/PlaylistDetailView.swift
+++ Echo.swiftpm/Echo/Views/PlaylistDetailView.swift
@@ -263,7 +263,7 @@ struct PlaylistDetailView: View {
                 deleteSelectedSongs()
             }
         } message: {
-            Text("remove_songs_confirmation_message \(selectedSongs.count)")
+            Text("\(selectedSongs.count) remove_songs_confirmation_message")
         }
     }
     
