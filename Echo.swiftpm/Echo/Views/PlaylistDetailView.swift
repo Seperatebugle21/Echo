ea9acff55db54f907f5108c04d03df98bbc0634c
Seperatebugle21 <151967822+Seperatebugle21@users.noreply.github.com>
2026-08-11 20:40:35 +0200

    Fix confirmation message for removing songs

--- Echo.swiftpm/Echo/Views/PlaylistDetailView.swift
+++ Echo.swiftpm/Echo/Views/PlaylistDetailView.swift
@@ -263,7 +263,7 @@ struct PlaylistDetailView: View {
                 deleteSelectedSongs()
             }
         } message: {
-            Text("remove_songs_confirmation_message \(selectedCount)")
+           Text("remove_songs_confirmation_message \(selectedSongs.count)")
         }
     }
     
