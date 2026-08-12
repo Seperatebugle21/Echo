6a3096ccd69c83bc7755bdf423be8998a4385eab
Seperatebugle21 <151967822+Seperatebugle21@users.noreply.github.com>
2026-08-12 23:27:51 +0200

    Update version from 1.0 to 2.0

--- Echo.swiftpm/Echo/Views/SettingsView.swift
+++ Echo.swiftpm/Echo/Views/SettingsView.swift
@@ -147,7 +147,7 @@ struct SettingsView: View {
                         
                         Spacer()
                         
-                        Text("1.0")
+                        Text("2.0")
                             .foregroundStyle(.secondary)
                     }
                     
